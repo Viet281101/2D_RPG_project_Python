09bc1030ae4551422ae60398068e44389c9b6d2b
Viet281101 <nva281101@gmail.com>
2022-02-09 22:23:02 +0100

    project

--- script/level.py
+++ script/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
 	def __init__(self):
@@ -31,7 +32,8 @@ class Level:
 		layouts = {
 			'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
 			'grass': import_csv_layout('../map/map_Grass.csv'),
-			'object': import_csv_layout('../map/map_LargeObjects.csv')
+			'object': import_csv_layout('../map/map_LargeObjects.csv'),
+			'entities': import_csv_layout('../map/map_Entities.csv')
 		}
 		graphics = {
 			'grass': import_folder('../graphics/grass'),
@@ -61,6 +63,28 @@ class Level:
 							Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 							# Tile((x, y), [self.obstacle_sprites], 'object', surf)
 
+						if style == 'entities':
+							if col == '394':
+								# collision map
+								self.player = Player((x, y), 
+									[self.visible_sprites], 
+									self.obstacle_sprites, 
+									self.create_attack, 
+									self.destroy_attack, 
+									self.create_magic)
+							else:
+								if col == '390': 
+									monster_name = 'bamboo'
+								elif col == '391': 
+									monster_name = 'spirit'
+								elif col == '392': 
+									monster_name = 'raccoon'
+								else: 
+									monster_name = 'squid'
+
+								Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacle_sprites)
+
+
 		### TEST:###
 		# 		if col == 'x':
 		# 			Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
@@ -68,13 +92,7 @@ class Level:
 		# 			self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
 
-		# collision map
-		self.player = Player((2000, 1400), 
-			[self.visible_sprites], 
-			self.obstacle_sprites, 
-			self.create_attack, 
-			self.destroy_attack, 
-			self.create_magic)
+		
 
 
 	def create_attack(self):
@@ -98,6 +116,7 @@ class Level:
 		# update and draw the game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
+		self.visible_sprites.enemy_update(self.player)
 		self.ui.display(self.player)
 		# debug(self.player.status)
 		# debug(self.player.direction)
@@ -131,4 +150,11 @@ class YSortCameraGroup(pygame.sprite.Group):
 		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
 			offset_pos = sprite.rect.topleft - self.offset
 			self.display_surface.blit(sprite.image, offset_pos)
+
+
+	def enemy_update(self, player):
+		enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+		for enemy in enemy_sprites:
+			enemy.enemy_update(player)
+
 
